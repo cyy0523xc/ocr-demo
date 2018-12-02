71b1c57c27063604c22697e59d01ca2093e33111
alex <cyy0523xc@gmail.com>
2018-12-02 21:47:30 +0800

    idcard

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@
 # Author: alex
 # Created Time: 2018年12月02日 星期日 19时41分39秒
 
-sudo docker run --rm -ti -p 8080:8080 \
+sudo docker rm -f ocr-py
+sudo docker run --rm -d --name=ocr-py \
+    -p 8080:8080 \
     -v /var/www/src/github.com/cyy0523xc/ocr-demo/static:/app/static \
     -v /var/www/src/github.com/cyy0523xc/ocr-demo/ocr.html:/app/templates/ocr.html \
     -v /var/www/src/github.com/cyy0523xc/ocr-demo/app_idcard.py:/app/app_idcard.py \

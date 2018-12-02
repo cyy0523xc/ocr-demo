da57d447eaadccbb520073f07b2a58f9b3262e61
alex <cyy0523xc@gmail.com>
2018-12-02 19:52:12 +0800

    start

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 # Created Time: 2018年12月02日 星期日 19时41分39秒
 
 sudo docker run --rm -ti -p 8080:8080 \
-    -v /var/www/src/github.com/layui-v2.4.5:/app/static \
-    -v /var/www/src/github.com/ocr.html:/app/templates/ocr.html \
-    -v /var/www/src/github.com/app_idcard.py:/app/app_idcard.py 
+    -v /var/www/src/github.com/cyy0523xc/ocr-demo/static:/app/static \
+    -v /var/www/src/github.com/cyy0523xc/ocr-demo/ocr.html:/app/templates/ocr.html \
+    -v /var/www/src/github.com/cyy0523xc/ocr-demo/app_idcard.py:/app/app_idcard.py 
     image_name /bin/bash

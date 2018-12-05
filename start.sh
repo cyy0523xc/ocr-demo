b1416a31602900a9e774c6098028255271d5e551
alex <cyy0523xc@gmail.com>
2018-12-05 12:00:44 +0800

    add test

--- start.sh
+++ start.sh
@@ -11,4 +11,5 @@ sudo docker run --rm -ti --name=ocr-py \
     -v /var/www/src/github.com/cyy0523xc/ocr-demo/static:/app/static \
     -v /var/www/src/github.com/cyy0523xc/ocr-demo/ocr.html:/app/templates/ocr.html \
     -v /var/www/src/github.com/cyy0523xc/ocr-demo/app_idcard.py:/app/app_idcard.py \
+    -v /var/www/src/github.com/cyy0523xc/ocr-demo/test.py:/app/test.py \
     $*

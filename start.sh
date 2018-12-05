6f41a1e061d07426abc78054cf166f0b6d38d467
alex <cyy0523xc@gmail.com>
2018-12-05 11:59:27 +0800

    add test

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 # Created Time: 2018年12月02日 星期日 19时41分39秒
 
 sudo docker rm -f ocr-py
-sudo docker run --rm -d --name=ocr-py \
+sudo docker run --rm -ti --name=ocr-py \
     -p 8080:8080 \
     -v /var/www/tmp/faces:/images \
     -v /var/www/src/github.com/cyy0523xc/ocr-demo/static:/app/static \
